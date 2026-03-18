4d78f0bb6383aeebb48e2db8032327b557f72893
fragass <fkp9wx6y@gmail.com>
2026-03-18 20:40:07 -0300

    Update admin-requests.js

--- api/admin-requests.js
+++ api/admin-requests.js
@@ -27,7 +27,7 @@ module.exports = async function handler(req, res) {
 
     const { data: adminUser, error: adminError } = await supabase
       .from("pusers")
-      .select("username, is_admin")
+      .select("username, its_admin")
       .eq("username", username)
       .maybeSingle();
 
@@ -38,7 +38,7 @@ module.exports = async function handler(req, res) {
       });
     }
 
-    if (!adminUser?.is_admin) {
+    if (!adminUser?.its_admin) {
       return res.status(403).json({
         success: false,
         message: "Acesso negado"
